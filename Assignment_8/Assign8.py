7c482f7adecd40bc6ac74a1a2c128d1cffe226af
Riya Aggarwal <riyaaggarwal0102@gmail.com>
2025-06-27 23:33:54 +0530

    uploading Assignment 8

---
+++ Assignment_8/Assign8.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+random_arr=np.random.rand(4,2)
+print(random_arr)    #random values
+empty_arr = np.empty((4,2)) #empty array
+print(empty_arr)
+full_arr = np.full((4,2),11) #full array
+print(full_arr)
+zero_arr = np.zeros((3,5))#zero array
+print(zero_arr)
+ones_arr = np.ones((4,3,2)) #ones array
+print(ones_arr)
